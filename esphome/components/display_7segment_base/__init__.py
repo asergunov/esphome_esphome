636bda5e87afab89cea0d0367d1155e1fabbef58
Anton Sergunov <setosha@gmail.com>
2024-04-29 19:19:07 +0000

    Fix a mistake

--- esphome/components/display_7segment_base/__init__.py
+++ esphome/components/display_7segment_base/__init__.py
@@ -1,7 +1,5 @@
 import esphome.codegen as cg
 
-display_7segment_base_ns = cg.esphome_ns.namespace(
-    "display_7segment_base", cg.PollingComponent
-)
-Display = display_7segment_base_ns.class_("Display")
+display_7segment_base_ns = cg.esphome_ns.namespace("display_7segment_base")
+Display = display_7segment_base_ns.class_("Display", cg.PollingComponent)
 DisplayRef = Display.operator("ref")
