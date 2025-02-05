ef8479f359887c370d1f61407f1e23d25eb0362c
Anton Sergunov <setosha@gmail.com>
2025-02-05 07:49:54 +0000

    logging

--- esphome/components/vs10x3/vs10x3_audio.cpp
+++ esphome/components/vs10x3/vs10x3_audio.cpp
@@ -9,8 +9,16 @@ void Vs10x3AudioComponent::setup() {
   ESP_LOGCONFIG(TAG, "Setting up vs10x3");
   // this->spi_setup(); // creashes
 
+  ESP_LOGV(TAG, "Creating driver");
   this->vs1053_ = make_unique<VS1053>(this->cs_pin_, this->dcs_pin_, this->dreq_pin_);
+  ESP_LOGV(TAG, "Initializing");
   this->vs1053_->begin();
+
+  if (this->vs1053_->isChipConnected()) {
+    ESP_LOGI(TAG, "Device connected. Version is %d", this->vs1053_->getChipVersion());
+  } else {
+    ESP_LOGE(TAG, "Device is not connected");
+  }
 }
 
 void Vs10x3AudioComponent::dump_config() {
@@ -20,8 +28,8 @@ void Vs10x3AudioComponent::dump_config() {
   ESP_LOGCONFIG(TAG, "   dreq_pin: %d", this->dreq_pin_);
 
   if (this->vs1053_) {
-    ESP_LOGCONFIG(TAG, "   chip conected: %d", this->vs1053->isChipConnected());
-    ESP_LOGCONFIG(TAG, "   chip version: %d", this->vs1053->getChipVersion());
+    ESP_LOGCONFIG(TAG, "   chip conected: %d", this->vs1053_->isChipConnected());
+    ESP_LOGCONFIG(TAG, "   chip version: %d", this->vs1053_->getChipVersion());
   }
 }
 
